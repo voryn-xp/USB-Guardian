fbe35fd012257fb765b6ede8e59c93de708a4622
virus_xp <virus_xx@tutamail.com>
2026-06-10 14:39:32 +0530

    fix viewmodel compose dependency reference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.lifecycle.viewmodel-compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 }
